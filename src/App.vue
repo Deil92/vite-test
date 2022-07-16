2a5d4e698c1b756ef0a8b1e8ae119e68bb1a7c0a
Vadim Khristoforov <khristoforov.vadim@yandex.ru>
2022-07-17 00:08:53 +0300

    Add linter

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,29 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+  export default {
+    mounted: () => {
+      console.log('hello');
+    },
+    methods: {
+      add() {
+        const a = '123';
+      },
+    },
+  };
 </script>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
 </style>
